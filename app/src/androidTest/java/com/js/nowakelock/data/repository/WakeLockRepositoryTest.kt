c70cdc5e0f2b7c39b849468033a81e3e0894670d
jasperhale <ljy087621@gmail.com>
2020-03-27 14:58:19 +0800

    WakeLockRepositoryTest

--- app/src/androidTest/java/com/js/nowakelock/data/repository/WakeLockRepositoryTest.kt
+++ app/src/androidTest/java/com/js/nowakelock/data/repository/WakeLockRepositoryTest.kt
@@ -4,8 +4,12 @@ import android.content.Context
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
+import com.js.nowakelock.LiveDataTestUtil
+import com.js.nowakelock.data.TestData
 import com.js.nowakelock.data.db.AppDatabase
+import kotlinx.coroutines.runBlocking
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -30,5 +34,69 @@ class WakeLockRepositoryTest {
     @After
     fun tearDown() {
         db.close()
+    }
+
+    @Test
+    fun upCount() {
+
+        runBlocking {
+            db.appInfoDao().insert(TestData.appInfos[0])
+        }
+
+        runBlocking {
+            wLR.upCount(TestData.pN, "w1")
+            wLR.upCount(TestData.pN, "w1")
+        }
+        assertEquals(wLR.wakeLocksHM["w1"]?.count, 2)
+    }
+
+    @Test
+    fun upBlockCount() {
+
+        runBlocking {
+            db.appInfoDao().insert(TestData.appInfos[0])
+        }
+
+        runBlocking {
+            wLR.upBlockCount(TestData.pN, "w1")
+            wLR.upBlockCount(TestData.pN, "w1")
+        }
+        assertEquals(wLR.wakeLocksHM["w1"]?.count, 2)
+        assertEquals(wLR.wakeLocksHM["w1"]?.blockCount, 2)
+    }
+
+    @Test
+    fun rstCount() {
+
+        runBlocking {
+            db.appInfoDao().insert(TestData.appInfos[0])
+        }
+
+        runBlocking {
+            wLR.upBlockCount(TestData.pN, "w1")
+            wLR.upBlockCount(TestData.pN, "w1")
+            wLR.rstCount(TestData.pN, "w1")
+        }
+        assertEquals(wLR.wakeLocksHM["w1"]?.count, 0)
+        assertEquals(wLR.wakeLocksHM["w1"]?.blockCount, 0)
+    }
+
+    @Test
+    fun insertAll() {
+
+        runBlocking {
+            db.appInfoDao().insert(TestData.appInfos[0])
+        }
+
+        runBlocking {
+            wLR.upBlockCount(TestData.pN, "w1")
+            wLR.upBlockCount(TestData.pN, "w1")
+            wLR.insertAll()
+        }
+        assertEquals(
+            LiveDataTestUtil.getValue(
+                db.wakeLockDao().loadAllWakeLocks(TestData.pN)
+            )[0].count, 2
+        )
     }
 }
