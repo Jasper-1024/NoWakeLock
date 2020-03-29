fd67c313ba6c10519a639c7ecb9c48d7ef149a85
jasperhale <ljy087621@gmail.com>
2020-03-29 17:33:01 +0800

    init BackService binding a notification.

--- app/src/main/java/com/js/nowakelock/ui/MainActivity.kt
+++ app/src/main/java/com/js/nowakelock/ui/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.js.nowakelock.ui
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -9,6 +14,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 import com.js.nowakelock.R
+import com.js.nowakelock.service.Service
 
 
 class MainActivity : AppCompatActivity() {
@@ -34,6 +40,10 @@ class MainActivity : AppCompatActivity() {
         findViewById<NavigationView>(R.id.nav_view)
             .setupWithNavController(navController)
 
+        //start BackService
+        startBackService()
+        //set NotificationChannel
+        createNotificationChannel()
     }
 
     private fun setupNavigationDrawer() {
@@ -41,5 +51,28 @@ class MainActivity : AppCompatActivity() {
             .apply {
                 setStatusBarBackground(R.color.colorPrimaryDark)
             }
+    }
+
+    private fun startBackService() {
+        val service = Intent(this, Service::class.java)
+        startService(service)
+    }
+
+    //for android version 8 and 8.1
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_MIN
+            val channel = NotificationChannel(getString(R.string.channel_id), name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
