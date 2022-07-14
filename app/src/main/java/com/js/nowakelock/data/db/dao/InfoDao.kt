9ac89a10332d99bbece3e129ee2ff3c42d8bbebf
JasperHale <ljy087621@gmail.com>
2022-07-14 14:32:11 +0800

    db
    - infoDao add userId

--- app/src/main/java/com/js/nowakelock/data/db/dao/InfoDao.kt
+++ app/src/main/java/com/js/nowakelock/data/db/dao/InfoDao.kt
@@ -7,43 +7,45 @@ import com.js.nowakelock.data.db.entity.Info
 @Dao
 interface InfoDao : BaseDao<Info> {
 
-    @Query("select * from info")
-    suspend fun loadInfos(): List<Info>
+    @Query("select * from info where userId = :userId")
+    suspend fun loadInfos(userId: Int = 0): List<Info>
 
-    @Query("select * from info where type_info = :type")
-    suspend fun loadInfos(type: Type): List<Info>
+    @Query("select * from info where type_info = :type and userId = :userId")
+    suspend fun loadInfos(type: Type, userId: Int = 0): List<Info>
 
-    @Query("select * from info where packageName_info = :packageName")
-    suspend fun loadInfos(packageName: String): List<Info>
+    @Query("select * from info where packageName_info = :packageName and userId = :userId")
+    suspend fun loadInfos(packageName: String, userId: Int = 0): List<Info>
 
     @Query("select * from info where packageName_info = :packageName and type_info = :type and userId = :userId")
     suspend fun loadInfos(packageName: String, type: Type, userId: Int = 0): List<Info>
 
-    @Query("select * from info where name_info = :name")
-    suspend fun loadInfo(name: String): Info?
+    @Query("select * from info where name_info = :name and userId = :userId")
+    suspend fun loadInfo(name: String, userId: Int = 0): Info?
 
-    @Query("select * from info where name_info = :name and type_info = :type")
-    suspend fun loadInfo(name: String, type: Type): Info?
+    @Query("select * from info where name_info = :name and type_info = :type and userId = :userId")
+    suspend fun loadInfo(name: String, type: Type, userId: Int = 0): Info?
 
-    @Query("update info set count = count+:count where name_info = :name and type_info = :type")
-    suspend fun upCount(count: Int, name: String, type: Type)
+    @Query("update info set count = count+:count where name_info = :name and type_info = :type and userId = :userId")
+    suspend fun upCount(count: Int, name: String, type: Type, userId: Int = 0)
 
-    suspend fun upCountPO(name: String, type: Type) = upCount(1, name, type)
+    suspend fun upCountPO(name: String, type: Type, userId: Int = 0) =
+        upCount(1, name, type, userId)
 
-    @Query("update info set blockCount = blockCount+:count where name_info = :name and type_info = :type")
-    suspend fun upBlockCount(count: Int, name: String, type: Type)
+    @Query("update info set blockCount = blockCount+:count where name_info = :name and type_info = :type and userId = :userId")
+    suspend fun upBlockCount(count: Int, name: String, type: Type, userId: Int = 0)
 
-    suspend fun upBlockCountPO(name: String, type: Type) = upBlockCount(1, name, type)
+    suspend fun upBlockCountPO(name: String, type: Type, userId: Int = 0) =
+        upBlockCount(1, name, type, userId)
 
-    @Query("update info set countTime = countTime+:time where name_info = :name and type_info = :type")
-    suspend fun upCountTime(time: Long, name: String, type: Type)
+    @Query("update info set countTime = countTime+:time where name_info = :name and type_info = :type and userId = :userId")
+    suspend fun upCountTime(time: Long, name: String, type: Type, userId: Int = 0)
 
 
-    @Query("update info set count = 0 where type_info = :type")
-    suspend fun rstAllCount(type: Type)
+    @Query("update info set count = 0 where type_info = :type and userId = :userId")
+    suspend fun rstAllCount(type: Type, userId: Int = 0)
 
-    @Query("update info set countTime = 0 where type_info = :type")
-    suspend fun rstAllCountTime(type: Type)
+    @Query("update info set countTime = 0 where type_info = :type and userId = :userId")
+    suspend fun rstAllCountTime(type: Type, userId: Int = 0)
 
     @Query("update info set count = 0")
     suspend fun rstAllCount()
